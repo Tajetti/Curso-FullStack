44c842b30681eb2d246a5fb9aa2c369298c55278
Tajetti <talhettialvaro@gmail.com>
2025-08-20 17:36:28 -0300

    substituindo e fatiando string

---
+++ JS-Intermediario/2. Métodos de texto/3. Substituindo e fatiando um texto/main.js
@@ -0,0 +1,8 @@
+let message = "Estou estudando sobre javascript"
+
+console.log(message)
+let messageAlt = message.replace("javascript", "python")
+console.log(messageAlt)
+
+let lorem = "Em uma pequena vila cercada por montanhas, vivia um jovem curioso chamado Lucas, que sonhava em explorar o mundo além do horizonte. Certo dia, ele encontrou um mapa antigo escondido no sótão de sua casa e, guiado pela coragem e pela esperança, embarcou em uma aventura que o levou a descobrir novos amigos, lugares mágicos e o verdadeiro significado de lar."
+console.log(lorem.replace("lar", " teste"))
