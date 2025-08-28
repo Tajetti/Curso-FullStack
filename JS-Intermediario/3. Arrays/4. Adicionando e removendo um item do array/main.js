0fc78c5b7461ac9e2fc5303ddff23a599539cd3e
Tajetti <talhettialvaro@gmail.com>
2025-08-28 15:36:33 -0300

    Realocando

--- JS-Intermediario/3. Arrays/4. Adicionando e removendo um item do array/main.js
+++ JS-Intermediario/3. Arrays/4. Adicionando e removendo um item do array/main.js
@@ -5,3 +5,10 @@ users.push("Alvaro")
 users.push("Rodrigo")
 users.push("Marcos")
 console.log(users)
+
+users.unshift("Laura")
+
+users.shift()
+console.log(users)
+users.pop()
+console.log(users)
---
+++ JS-Intermediario/3. Arrays/5. Usando o índice/main.js
@@ -0,0 +1,7 @@
+let fruits = ["Apple", "Orange", "Watermelon"]
+
+let position = fruits.indexOf("Watermelon")
+console.log(position)
+
+fruits.splice(1, 2)
+console.log(fruits)
