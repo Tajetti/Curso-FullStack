05d3e5245aa1410510d8480931d2fd35a7874ac8
Tajetti <talhettialvaro@gmail.com>
2025-08-28 17:53:41 -0300

    Aprendendo sobre loop infinito, DoWhile, For e For in

---
+++ JS-Intermediario/4. Iteração e repetição/2. Loop Do While/main.js
@@ -0,0 +1,7 @@
+let value = 0
+
+do{
+    value++
+    console.log(value)
+}while(value <= 10)
+console.log("continua fluxo")
---
+++ JS-Intermediario/4. Iteração e repetição/3. Loop for/main.js
@@ -0,0 +1,4 @@
+console.log("-- Multiplicação do 7 --")
+for(number = 0; number <= 10; number++){
+    console.log(`7 x ${number} = ${7*number}`)
+}
